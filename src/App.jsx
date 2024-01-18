8f4221f631f74a8a27cde3dbe14c67c6d1c401c5
gonzalote99 <62606335+gonzalote99@users.noreply.github.com>
2024-01-18 21:36:37 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React, {createContext, useState} from 'react';
+import './App.css';
+import styled from 'styled-components';
+import Wrapper from './wrapper';
+import Table from './table';
+import Header from './header';
+import Rules from './rules';
+
+export const ScoreContext = createContext();
+
+const AppStyled = styled.main `
+background-image: radial-gradient(circle at top, #1F3757 20%, #131537 100%);
+color: white;
+font-family: 'Barlow Semi Condensed', sans-serif;
+
+.app-content {
+  padding: 2rem;
+  min-height: 100vh;
+  display: flex;
+  box-sizing: border-box;
+  flex-direction: column;
+  justify-content: space-between;
+}
+`
+function App() {
+  const [score, setScore] = useState(0);
+  return(
+    <ScoreContext.Provider value={{
+      score, setScore
+    }}>
+    <AppStyled>
+    <Wrapper>
+    <div className='app-content'>
+    <Header />
+      <Table />
+      <Rules />
+    </div>
+    </Wrapper>
+    </AppStyled>
+    </ScoreContext.Provider>
+  );
+}
+
+export default App;
